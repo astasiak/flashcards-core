c7f6d0696859eca60b3cf6dddf8d5e8b7dc53327
criceto-pigro <andst4@gmail.com>
2015-03-07 16:05:50 +0100

    heroku mongoLab mongodb connection

--- src/main/java/main/SpringMongoConfig.java
+++ src/main/java/main/SpringMongoConfig.java
@@ -1,5 +1,6 @@
 package main;
 
+import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -12,21 +13,32 @@ import com.mongodb.MongoClientURI;
 @Configuration
 public class SpringMongoConfig extends AbstractMongoConfiguration {
 	
-	private final String DEFAULT_MONGO_URI = "mongodb://localhost";
+	private static final Logger LOG = Logger.getLogger(SpringMongoConfig.class);
+	
+	private final String DEFAULT_MONGO_URI = "mongodb://localhost/yourdb";
+	
+	private MongoClientURI mongoUri;
 	
 	@Override
 	public String getDatabaseName() {
-		return "yourdb";
+		return getMongoUri().getDatabase();
 	}
  
 	@Override
 	@Bean
 	public Mongo mongo() throws Exception {
-		String uriString = System.getenv("MONGOLAB_URI");
-		if(StringUtils.isEmpty(uriString)) {
-			uriString = DEFAULT_MONGO_URI;
+		return new MongoClient(getMongoUri());
+	}
+	
+	private MongoClientURI getMongoUri() {
+		if(mongoUri==null) {
+			String uriString = System.getenv("MONGOLAB_URI");
+			if(StringUtils.isEmpty(uriString)) {
+				uriString = DEFAULT_MONGO_URI;
+			}
+			LOG.info("Connecting to mongo instance: ["+uriString+"]");
+			mongoUri = new MongoClientURI(uriString);
 		}
-		MongoClientURI uri  = new MongoClientURI(uriString);
-		return new MongoClient(uri);
+		return mongoUri;
 	}
 }
